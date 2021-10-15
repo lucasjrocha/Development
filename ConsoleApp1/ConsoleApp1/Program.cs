f2df85a76d0ce016dc216b883fd68fb4d3767328
Lucas Rocha <lucasjcsr@outlook.com>
2021-10-15 16:45:44 -0300

    teste3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,9 +10,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Qual seu nome?");
-            string nome = Console.ReadLine();
-            Console.WriteLine("Seu nome é " + nome);
+            // 1.1 Tipos de Variáveis em C# 
+            /* 
+            int segGuerrMund = 1942;
+            string cor_favorita = "Vermelho";
+            float velocidadeF1 = 194.48f;
+            bool segGuerraMundHappened = true;
+            */
+            // 1.2 Exemplos de uso das variáveis
+            /*
+            Console.WriteLine(segGuerrMund);
+            Console.WriteLine(cor_favorita);
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(segGuerraMundHappened);
+
+            Console.WriteLine("Valor Antigo: " + velocidadeF1);
+            Console.WriteLine("Valor Novo: " + (velocidadeF1 = 348.29f));
+            */
+
+            /*
+            //1.4 Variavel do tipo var, nao precisa pre-definir o tipo dela, 
+            //como string cor_favorita = "Vermelho";, pois ela recebe 
+            //o tipo do dado que estiver sendo adicionado à ela automaticamente.
+
+            var cor_favorita = "Vermelho";
+            var numero = 12312;
+             */
+            //1.3 Variável do tipo dinamica, pode obter qualquer valor.
+            dynamic cor_favorita = "Vermelho";
+
+            Console.WriteLine(cor_favorita);
+
+            cor_favorita = 1203;
+
+            Console.WriteLine(cor_favorita);         
+
+
+
+            Console.ReadLine();
         }
     }
 }
